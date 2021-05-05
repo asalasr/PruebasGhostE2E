3e8b7a97d684a7c4ff8404fb7ae28167391848ff
johanCarvajalAndes <j.carvajalm@uniandes.edu.co>
2021-05-05 17:10:13 -0500

    escenario 1 post

--- cypress/integration/post/listFuction.js
+++ cypress/integration/post/listFuction.js
@@ -1,7 +1,44 @@
-export function login(){
-    cy.visit('http://localhost:2368/ghost/#/signin')
-    cy.get('.email').type('j.carvajalm@uniandes.edu.co')
-    cy.get('.password').type('mfYyHi8q.Mix@r#')
-    cy.get('.login').click()
-  }
+export function login(cy) {
+  cy.visit("http://localhost:2368/ghost/#/signin");
+  cy.get(".email").type("j.carvajalm@uniandes.edu.co");
+  cy.get(".password").type("mfYyHi8q.Mix@r#");
+  cy.get(".login").click();
+}
+
+export function newPostPage(cy) {
+  cy.get('a[href="#/posts/"]').click({ force: true });
+  cy.get('a[href="#/editor/post/"]').click({ force: true });
+}
+
+export function newPostTitle(cy,title) {
+  cy.get('textarea[tabindex="1"]').type(title);
+  cy.get(
+    'div[data-kg="editor"]'
+  ).click();
+}
+
+export function newPostBody(cy,text) {
+  cy.get(
+    'div[data-kg="editor"]'
+  ).type(text);
+}
+
+
+export function returnPostList(cy) {
+ 
+  cy.get('a[href="#/posts/"]').then($links => {
+    var randomLink = $links.get(1);
+        randomLink.click({force: true});
   
+    cy.wait(1000);
+  
+});
+
+
+}
+
+
+export function verifyPostTitle(cy, title) {
+ 
+  cy.get('h3').contains(title)
+}
--- cypress/integration/post/post.spec.js
+++ cypress/integration/post/post.spec.js
@@ -1,23 +1,29 @@
 /// <reference types="cypress" />
-import {login} from './listFuction.js'
-context('Post Escenarios', () => {
+import { login, newPostPage, verifyPostTitle,newPostTitle,returnPostList } from "./listFuction.js";
+context("Post Escenarios", () => {
+  let title;
   beforeEach(() => {
-    
-   login()
-  })
+    //iniciar sesion
+    title= "titleTest"
+    login(cy);
+  });
 
-  it('Escenario 1 crear un post', () => {
-    // https://on.cypress.io/go
-    //cy.url().should('include', '/users/1/edit')
-    //click en post
-    cy.get('a[href="#/posts/"]').click({force:true})
-
+  it("Escenario 1 crear un post como borrador con titulo", () => {
+     
+    //ir a la pagina de crear new Post
+    newPostPage(cy);
+    //escribir el titulo
+    newPostTitle(cy,title);
+    //volver a lista de post
+    returnPostList(cy)
+    //verificar titulo creado
+    verifyPostTitle(cy, title)
     
-  })
+  });
 
   // it('cy.reload() - reload the page', () => {
   //   // https://on.cypress.io/reload
-    
+
   // })
 
   // it('cy.visit() - visit a remote url', () => {
@@ -26,8 +32,6 @@ context('Post Escenarios', () => {
   //   // Visit any sub-domain of your current domain
 
   //   // Pass options to the visit
-    
-  //   })
-  
-})
 
+  //   })
+});
