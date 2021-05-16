47f4c4fa2d88358fa3aa589f003ab9e02d14d14a
AndresSalas <a.salasr@uniandes.edu.co>
2021-05-15 20:03:24 -0500

    se cargan screenshot 3.3.0

--- cypress/integration/tags/listFuctionTags.js
+++ cypress/integration/tags/listFuctionTags.js
@@ -4,16 +4,24 @@ export function login(cy,url,email,password) {
   cy.get(".password").type(password);
   cy.get(".login").click();
 }
-export function newTag(cy,url,title) {  
+export function Tag(cy,url,title) {  
   cy.get('a[href="#/tags/"]').click({ force: true });
-  cy.get('a[href="#/tags/new/"]').click({ force: true });
 }
 
-export function saveTag(cy,url,title) {  
+export function newTag(cy,url,title) {  
+  cy.get('a[href="#/tags/new/"]').click({ force: true });
+  
+}
+
+export function formTag(cy,title) {  
   cy.get('input[name=name]').click({ force: true });
   cy.get('input[name=name]').type(title);
   cy.get('input[name=slug]').click({ force: true });
   cy.get('input[name=slug]').type(title);
+  //cy.get('button[class="gh-btn gh-btn-blue gh-btn-icon ember-view"]').click({ force: true });
+}
+
+export function saveTag(cy) {
   cy.get('button[class="gh-btn gh-btn-blue gh-btn-icon ember-view"]').click({ force: true });
 }
 
@@ -38,15 +46,21 @@ export function NewPage(cy,url,page,title){
 }
 
 export function verifyTagEnableInPage(cy,url,page,title){
-  cy.get('a[href="#/pages/"]').click({ force: true });
+  //cy.get('a[href="#/pages/"]').click({ force: true });
+  //cy.get('div[class="f8 fw3 lh-copy tracked-2 ma0 pa0 h9 br b--lightgrey pl3 pr4 flex items-center br2 br--left bg-white"]').click({ force: true })
   cy.get("h3").contains(title).click({ force: true });
   cy.get('button[class="post-settings"]').click({ force: true })
   cy.get('div[id=tag-input]').click({ force: true });
   cy.get('li[class="ember-power-select-option"]').contains(title).click({force:true});
 }
 
-export function selectTag(cy,title) {  
-  cy.get('a[href="#/tags/"]').click({ force: true });
+export function deletePage(cy){
+  cy.get('button[class="gh-btn gh-btn-hover-red gh-btn-icon settings-menu-delete-button"]').click({ force: true })
+  wait()
+  cy.get('button[class="gh-btn gh-btn-red gh-btn-icon ember-view"]').click({ force: true })
+}
+
+export function selectTag(cy,title) {
   cy.get("h3").contains(title).click({ force: true });
   cy.get('button[class="gh-btn gh-btn-red gh-btn-icon mb15"]').click({ force: true });
 }
@@ -65,4 +79,8 @@ export function wait() {
 
 export function irA(cy,url,subUrl){
   cy.visit(url + subUrl);
+}
+
+export function photo(cy,scenario,step){
+  cy.screenshot(scenario + "/" + step);
 }
--- cypress/integration/tags/tags.spec.js
+++ cypress/integration/tags/tags.spec.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-import { login,newTag,deleteTag,verifyTagEnableInPost,verifyTagEnableInPage,verifyTaginReaderWeb,wait,saveTag,irA,goPost,NewPage,selectTag } from "./listFuctionTags.js";
+import { login,newTag,deleteTag,verifyTagEnableInPost,verifyTagEnableInPage,verifyTaginReaderWeb,wait,saveTag,irA,goPost,NewPage,selectTag,Tag,photo ,formTag,deletePage} from "./listFuctionTags.js";
 import {Email, Password, Url} from '../credencia'
 context("Tags Escenarios", () => {
   //datos acceso
@@ -18,62 +18,98 @@ context("Tags Escenarios", () => {
     	post = "Welcome";
     	page = "Hola";
   		tag = "Getting Started";
-  		//iniciar session
+		//iniciar session
     	login(cy,url,email,password);
   });
   
   it("Given: Estando loggeado exitosamente en la aplicación. WHEN: proceso al listado d tags. " +
         "THEN: creo un nuevo tag.", () => {
-  		//crear nuevo tag
+  		//tag
+    	Tag(cy,url,title);
+		photo(cy,"escenario1","step1");
+		//crear nuevo tag
     	newTag(cy,url,title);
-    	//espera
+		wait();
+		photo(cy,"escenario1","step2");
    		wait();
-   		//guardad tag
-   		saveTag(cy,url,title);
+	 	//Formulario tag
+		formTag(cy,title);
+		wait();
+	    photo(cy,"escenario1","step3");
+   		//guardar tag
+   		saveTag(cy);
+		wait();
+	    photo(cy,"escenario1","step4");
    		//redireccionar
     	irA(cy,url,"ghost/#/tags");
+		wait();
+		photo(cy,"escenario1","step5");
   });
   
-  it("Given: Estando loggeado exitosamente en la aplicación. WHEN: proceso a un post. " +
+ it("Given: Estando loggeado exitosamente en la aplicación. WHEN: proceso a un post. " +
         "THEN: se valida que el tag este habilitado.", () => {
   		//ir a un Post
      	goPost(cy,post,title);
+		wait();
+		photo(cy,"escenario2","step1");
      	//espera
      	wait();
      	//Valida el tag habilitado en los post
 		verifyTagEnableInPost(cy,post,title);
+		wait();
+		photo(cy,"escenario2","step2");
   });
 
   it("Given: Estando loggeado exitosamente en la aplicación. WHEN: proceso a una Page. " +
         "THEN: se valida que el tag este habilitado.", () => {
 	    //nueva Page
 		NewPage(cy,url,page,title);
+		wait();
+		photo(cy,"escenario3","step1");
 		//espera
      	wait();
      	//redireccionar
      	irA(cy,url,"ghost/#/pages");
+		wait();
+		photo(cy,"escenario3","step2");
      	//Valida el tag habilitado en las pages
 		verifyTagEnableInPage(cy,url,page,title);
+		wait();
+		photo(cy,"escenario3","step3");
+		//borramos pagina creada
+		deletePage(cy);
+		photo(cy,"escenario3","step4");
   });
 
   it("Given: Estando loggeado exitosamente en la aplicación. WHEN: proceso al listado de tags. " +
         "THEN: se selecciona el tag y se elimina.", () => {
+		//tags
+		Tag(cy,url,title);
+		wait();
+		photo(cy,"escenario4","step1");
   		//seleccionar un tag
         selectTag(cy,title);
+		wait();
+		photo(cy,"escenario4","step2");
         //espera
      	wait();
      	//borrar el tag
 		deleteTag(cy,title);
+		photo(cy,"escenario4","step3");
  });
 
  it("Given: Se ingresa al web de lectura. WHEN: se busca el post con el tag asociado" +
         "THEN: verificado el tag se selecciona", () => {
 		//redireccionar
     	irA(cy,url,post);
+		wait();
+		photo(cy,"escenario5","step1");
     	//espera
      	wait();
      	//validar tag en el post del sitio web
 		verifyTaginReaderWeb(cy,url,post,tag);
+		wait();
+		photo(cy,"escenario5","step2");
  });
 
 });
