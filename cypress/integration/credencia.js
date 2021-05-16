cf7c4dc0a46919a74e1bbbc7ce5e163a67f5f9ac
johanCarvajalAndes <j.carvajalm@uniandes.edu.co>
2021-05-15 19:01:56 -0500

    pruebas finalizadas

--- cypress/integration/credencia.js
+++ cypress/integration/credencia.js
@@ -1,4 +1,4 @@
 export const Email = "j.carvajalm@uniandes.edu.co"
-export const Password = "mfYyHi8q.Mix@r#"
-export const Url = "http://localhost:2368"
+export const Password = "jQ6KHZdu5wf!Fd!"//jQ6KHZdu5wf!Fd!//mfYyHi8q.Mix@r#
+export const Url = "http://localhost:2369"
 export const User= "Johan Fernando Carvajal Marín"
--- cypress/integration/post/post.spec.js
+++ cypress/integration/post/post.spec.js
@@ -5,10 +5,10 @@ context("Post Escenarios", () => {
   let title;
   let email = Email;
   let password = Password;
-  
+  let escenario = "NO"
   beforeEach(() => {
     //Given: Estando logeados en la aplicacion
-    title= "lapruebae3243"
+    title= "lapruebaReina"
     login(cy,email,password);
   });
 
@@ -17,28 +17,30 @@ context("Post Escenarios", () => {
   "WHEN escribir el titulo" +
   "WHEN volver a lista de post" +
   "THEN: verificar post en lista de post.", () => {
-     
+    escenario = "escenario1/crearnewPost/"
     //WHEN ir a la pagina de crear new Post
-    newPostPage(cy);
+    newPostPage(cy,escenario);
+  
+  
     //WHEN escribir el titulo
-    newPostTitle(cy,title);
+    newPostTitle(cy,title,escenario);
     //WHEN volver a lista de post
-    returnPostList(cy)
+    returnPostList(cy,escenario)
     //THEN verificar post en lista de post
-    verifyPostTitle(cy, title)
+    verifyPostTitle(cy, title,escenario)
     
   });
 it("Given: Estando loggeado exitosamente en la aplicación."+ 
 "WHEN seleccionar el post" +
 "WHEN ir a configuraciones del post" +
 "THEN: eliminar el post.", () => {
-     
+  escenario = "escenario2/EliminarBorradorPost"
     //WHEN seleccionar el post
-   selectPost(cy,title);
+   selectPost(cy,title,escenario);
     // WHEN ir a configuraciones del post
-    configPost(cy);
+    configPost(cy,escenario);
    //THEN eliminar el post
-    deletePost(cy)
+    deletePost(cy,escenario)
     
   });
 
@@ -46,54 +48,58 @@ it("Given: Estando loggeado exitosamente en la aplicación."+
       "WHEN crear un nuevo Post" +
       "WHEN publicarlo" +
       "THEN: verificar el post en la web page.", () => {
+        escenario = "escenario3/PublicarPost"
      //WHEN ir a la pagina de crear new Post
-    newPostPage(cy);
+    newPostPage(cy,escenario);
     //WHEN escribir el titulo
-    newPostTitle(cy,title);
+    newPostTitle(cy,title,escenario);
     //WHEN volver a lista de post
-    returnPostList(cy)
+    returnPostList(cy,escenario)
     //WHEN seleccionar el post creado
-    retuntToPost(cy,title);
+    retuntToPost(cy,title,escenario);
     //WHEN publicar el post
-    publicPost(cy);
+    publicPost(cy,escenario);
    // THEN verificar post en web page
-    verifyPostTitleinWeb(cy, title)
+    verifyPostTitleinWeb(cy, title,escenario)
     
   });
 
   it("Given: Estando loggeado exitosamente en la aplicación."+
   "WHEN ir a un post publicado y verificar el slug" +
   "THEN el post esta publicado en esa ruta", () => {
+    escenario = "escenario4/VerificarSlugPost"
     //WHEN ir a un post publicado 
-    goToPostPublished(cy, title)
+    goToPostPublished(cy, title,escenario)
    //WHEN ir a configuraciones
-   configPost(cy);
+   configPost(cy,escenario);
   //WHEN verificar y extraer la ruta asignada al post
-   verifySlug(cy)
+   verifySlug(cy,escenario)
     //THEN verificar titulo del post en la web page
-   verifyPageOnePost(cy,title)
+   verifyPageOnePost(cy,title,escenario)
    
  });
 
  it("Given: Estando loggeado exitosamente en la aplicación."+
  "WHEN ir a la pagina principal de post publicados" +
  "THEN verificar que el usuario cuenta con el post en sus publicaciones", () => {
+  escenario = "escenario5/VerificarUsuarioPost"
   // WHEN ir a la pagina principal de post publicados
- verifyPostTitleinWeb(cy, title)
+ verifyPostTitleinWeb(cy, title,escenario)
  //THEN verificar que el usuario cuenta con el post en sus publicaciones
- verifyUserListPost(cy, title)
+ verifyUserListPost(cy, title,escenario)
  
 });
 
 it("Given: Estando loggeado exitosamente en la aplicación."+
 "WHEN ir a un post publicado" +
 "THEN eliminar el post", () => {
+  escenario = "escenario6/EliminarPostPublicado"
   //WHEN ir a un post publicado
- goToPostPublished(cy, title)
+ goToPostPublished(cy, title,escenario)
  //WHEN ir a configuraciones del post
- configPost(cy);
+ configPost(cy,escenario);
  //THEN eliminar el post
-  deletePost(cy)
+  deletePost(cy,escenario)
  
 });
 
